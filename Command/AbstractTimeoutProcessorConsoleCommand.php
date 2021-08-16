9dc9bdda0bb35f3ddf2642f2deb980996e71457b
Morebot <info@morebec.com>
2021-08-16 19:33:06 +0000

    [Modeling] Add EntityIdTrait

---
+++ Command/AbstractTimeoutProcessorConsoleCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Morebec\OrkestraSymfonyBundle\Command;
+
+use Morebec\Orkestra\Messaging\Timeout\TimeoutProcessorInterface;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Command\SignalableCommandInterface;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * Abstract implementation of a console command intended to run a timeout processor.
+ */
+abstract class AbstractTimeoutProcessorConsoleCommand extends Command implements SignalableCommandInterface
+{
+    protected static $defaultName = 'orkestra:timeout-processor';
+
+    /**
+     * Initialized on execute.
+     */
+    private SymfonyStyle $io;
+
+    public function getSubscribedSignals(): array
+    {
+        return [\SIGTERM, \SIGINT];
+    }
+
+    public function handleSignal(int $signal): void
+    {
+        $this->io->writeln('Timeout Processor Stopping ...');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $this->io = new SymfonyStyle($input, $output);
+
+        $this->io->title('Timeout Processor');
+
+        $processor = $this->getTimeoutProcessor();
+
+        $this->io->writeln('Timeout Processor Started.');
+        $processor->start();
+
+        $this->io->writeln('Timeout Processor Stopped.');
+
+        return self::SUCCESS;
+    }
+
+    /**
+     * Returns the processor to use for this console command.
+     */
+    abstract protected function getTimeoutProcessor(): TimeoutProcessorInterface;
+}
